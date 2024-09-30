bf68bff62ae255c9c926ef1f26f206bd5f31cb14
viveksharma19110 <viveksharma19110@gmail.com>
2024-09-30 09:29:02 +0530

    7 th commit

--- app/components/CartIcon.tsx
+++ app/components/CartIcon.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import { ShoppingCart } from 'lucide-react';
 
 interface CartIconProps {
@@ -11,14 +10,11 @@ const CartIcon: React.FC<CartIconProps> = ({ itemCount }) => {
     <div className="relative">
       <ShoppingCart className="h-6 w-6 text-white" />
       {itemCount > 0 && (
-        <motion.span
+        <span
           className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs"
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ type: 'spring', stiffness: 500, damping: 30 }}
         >
           {itemCount}
-        </motion.span>
+        </span>
       )}
     </div>
   );
